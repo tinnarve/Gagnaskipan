5b66808f55e425d594f9c9d0f50ef579d6a3234f
Kolfinna Jónsdóttir <kolfinna17@ru.is>
2018-01-28 19:49:30 +0000

    h files

---
+++ Forritunarverkefni 2/V2_QueueAndStack/queue.h
@@ -0,0 +1,20 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+using namespace std;
+
+class Queue
+{
+    public:
+        Queue ;
+        virtual ~Queue(){};
+
+    protected:
+
+    private:
+};
+
+
+
+#endif // QUEUE_h
+
---
+++ Forritunarverkefni 2/V2_QueueAndStack/stack.h
@@ -0,0 +1,19 @@
+#ifndef STACK_H
+#define STACK_H
+
+using namespace std;
+
+class Stack
+{
+    public:
+        Stack ;
+        virtual ~Stack(){};
+
+    protected:
+
+    private:
+};
+
+
+
+#endif // STACK_h
