4733ae63f303c5161a6d73bfe55d23e176273fb9
Kolfinna Jónsdóttir <kolfinna17@ru.is>
2018-01-28 21:34:26 +0000

    stack auka

--- Forritunarverkefni 2/V2_QueueAndStack/stack.h
+++ Forritunarverkefni 2/V2_QueueAndStack/stack.h
@@ -1,13 +1,23 @@
 #ifndef STACK_H
 #define STACK_H
-
+#include "measurement.h"
 using namespace std;
 
 class Stack
 {
     public:
-        Stack ;
+        Stack(){};
         virtual ~Stack(){};
+
+        void push(Measurement data);
+        Measurement pop();
+        int size();
+        bool isEmpty();
+
+        friend ostream& operator <<(ostream &out, const Stack s){//held það sé Stack ekki viss
+            //útfærsla
+            return out;
+        };
 
     protected:
 
