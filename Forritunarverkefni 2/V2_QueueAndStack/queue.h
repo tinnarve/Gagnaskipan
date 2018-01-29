91f9caf40e82e62bbc6ba9bac4bff7fc9e8393d3
tinnarve <34030683+tinnarve@users.noreply.github.com>
2018-01-29 00:17:56 +0000

    Queue

--- Forritunarverkefni 2/V2_QueueAndStack/queue.h
+++ Forritunarverkefni 2/V2_QueueAndStack/queue.h
@@ -1,15 +1,22 @@
 #ifndef QUEUE_H
 #define QUEUE_H
+#include "measurement.h"
 
 using namespace std;
+
+class EmptyException();
 
 class Queue
 {
     public:
-        Queue(){} ;
-        virtual ~Queue(){};
+        Queue();
+        virtual ~Queue();
 
-    protected:
+        void add(Measurement data);
+        Measurement remove();
+        int size();
+        bool isEmpty();
+        friend ostream &operator << (ostream&, const Queue q);
 
     private:
 };
