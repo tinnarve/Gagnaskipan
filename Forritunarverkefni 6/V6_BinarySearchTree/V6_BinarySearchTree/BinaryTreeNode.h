08df37eadc1250bbd5e8478b860a05543263adea
unknown <tinnarut96@gmail.com>
2018-03-07 17:42:00 +0000

    node

--- Forritunarverkefni 6/V6_BinarySearchTree/V6_BinarySearchTree/BinaryTreeNode.h
+++ Forritunarverkefni 6/V6_BinarySearchTree/V6_BinarySearchTree/BinaryTreeNode.h
@@ -1,16 +1,16 @@
 #ifndef BINARYTREENODE_H
 #define BINARYTREENODE_H
-
+#include <stdio.h>
 
 class BinaryTreeNode
 {
     public:
-        BinaryTreeNode() {}
-        virtual ~BinaryTreeNode() {}
-
-    protected:
-
-    private:
+        BinaryTreeNode(char data = '0', BinaryTreeNode *left = NULL, BinaryTreeNode *right = NULL :
+                       data(data), left(left), right(right)) {};
+        virtual ~BinaryTreeNode() {};
+        char data;
+        BinaryTreeNode *left;
+        BinaryTreeNode *right;
 };
 
 #endif // BINARYTREENODE_H
