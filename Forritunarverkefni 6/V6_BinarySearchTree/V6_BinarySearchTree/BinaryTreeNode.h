3e35171f646d8aa3e466e9be3b1843abc9c455ed
unknown <tinnarut96@gmail.com>
2018-03-07 17:25:52 +0000

    node

---
+++ Forritunarverkefni 6/V6_BinarySearchTree/V6_BinarySearchTree/BinaryTreeNode.h
@@ -0,0 +1,16 @@
+#ifndef BINARYTREENODE_H
+#define BINARYTREENODE_H
+
+
+class BinaryTreeNode
+{
+    public:
+        BinaryTreeNode() {}
+        virtual ~BinaryTreeNode() {}
+
+    protected:
+
+    private:
+};
+
+#endif // BINARYTREENODE_H
