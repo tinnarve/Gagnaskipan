49d9443442a647c6982b349bd7c0dcef0c6920d9
unknown <tinnarut96@gmail.com>
2018-03-07 23:38:30 +0000

    BSTMap

--- Forritunarverkefni 6/V6_BinarySearchTree/V6_BinarySearchTree/BSTMap.h
+++ Forritunarverkefni 6/V6_BinarySearchTree/V6_BinarySearchTree/BSTMap.h
@@ -12,12 +12,21 @@ class BSTMap : public Map<K, T>
     public:
         BSTMap() {
             root = NULL;
+            numItems = 0;
         }
         virtual ~BSTMap() {
             clear();
         }
         virtual void insert(K key, T data) {
-
+            BinaryTreeNode<K,T>* &node = find(key, root);
+            if(node == NULL)
+            {
+                node = new BinaryTreeNode<K,T>(key, data);
+            }
+            else{
+            BinaryTreeNode<K,T>* node = new BinaryTreeNode<K,T>(key, data, NULL, NULL);
+            numItems++;
+            }
         }
 
         virtual void update(K key, T data) {
@@ -37,26 +46,53 @@ class BSTMap : public Map<K, T>
             }
         }
 
+        void nodeRemove(BinaryTreeNode<K, T>* &node) {
+            if(node->left == NULL && node->right == NULL) {
+                delete node;
+                node = null;
+            }
+            else if(node->right == NULL) {
+                BinaryTreeNode<K, T>* tmp = node;
+                node = node->left;
+                delete tmp;
+            }
+            else if(node->left == NULL) {
+                BinaryTreeNode<K, T>* tmp = node;
+                node = node->right;
+                delete tmp;
+            }
+            else {
+               BinaryTreeNode* &nodeForDeletion = findRightMostNode(node->left);
+               node->data = nodeForDeletion->data;
+               removeNode(nodeForDeletion);
+            }
+        }
+
+        findRightMostNode(BinaryTreeNode* &node) {
+
+        }
+
         virtual bool contains(K key) {
 
         }
 
-        BinaryTreeNode<K, T>* find(K key, BinaryTreeNode<K, T>* &node) {
+        BinaryTreeNode<K, T>* &find(K key, BinaryTreeNode<K,T>* &node)
+        {
             if(node == NULL){
                 return node;
             }
-            if(key < node->data)
+            if(key < node->key)
             {
                 return find(key, node->left);
             }
-            else if(node->data < key){
+            else if(node->key < key){
                 return find(key, node->right);
             }
             return node;
         }
 
         virtual int size() const {
-
+            return numItems;
         }
 
         virtual bool empty() const {
@@ -69,11 +105,20 @@ class BSTMap : public Map<K, T>
         }
 
         virtual void print(ostream& out) const {
-
+            print(root);
         }
 
     private:
         BinaryTreeNode<K, T> *root;
+        int numItems;
+
+        void print(BinaryTreeNode<K,T>* node) const {
+            if(node != NULL) {
+                print(node->left);
+                cout << node->data << " ";
+                print(node->right);
+            }
+        }
 };
 
 #endif // BSTMAP_H
